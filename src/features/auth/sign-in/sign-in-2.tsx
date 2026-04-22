4873d5a9c87bc9b4dd164d662d05a733fcf631af
SuperWindCloud <19338823723@163.com>
2026-04-22 17:34:33 +0800

    chore:   code structure and remove redundant sections for improved readability and maintainability

--- src/features/auth/sign-in/sign-in-2.tsx
+++ src/features/auth/sign-in/sign-in-2.tsx
@@ -62,14 +62,14 @@ export function SignIn2() {
           className='dark:hidden'
           width={1024}
           height={1151}
-          alt='Shadcn-Admin'
+          alt='Vite Shadcn Template'
         />
         <img
           src={dashboardDark}
           className='hidden dark:block'
           width={1024}
           height={1138}
-          alt='Shadcn-Admin'
+          alt='Vite Shadcn Template'
         />
       </div>
     </div>
